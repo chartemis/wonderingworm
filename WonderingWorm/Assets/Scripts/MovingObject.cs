91c3d35dfa7363626262d6695c744bf149658303
Charles Hart <hartcp8@gmail.com>
2018-07-28 15:06:36 -0400

    Basic movement of the character

--- WonderingWorm/Assets/Scripts/MovingObject.cs
+++ WonderingWorm/Assets/Scripts/MovingObject.cs
@@ -2,16 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MovingObject : MonoBehaviour {
+public abstract class MovingObject : MonoBehaviour {
 
-	public BoxCollider2D boxCollider;  
-	public Rigidbody2D rb2D;           
+	private BoxCollider2D boxCollider;  
+	private Rigidbody2D rb2D;           
 	public LayerMask blockingLayer;         //Layer on which collision will be checked.
 
+    private float inverseMoveTime;          //Used to make movement more efficient.
 	
+    public float moveTime;         //Time it will take object to move, in seconds.
+
+	public float moveSpeed;
+
 	// Use this for initialization
-	void Start () {
-		
+	protected virtual void Start () {
+			moveTime = 0.075f;
+			moveSpeed = .05f;
+
+            //Get a component reference to this object's BoxCollider2D
+            boxCollider = GetComponent<BoxCollider2D>();
+
+            //Get a component reference to this object's Rigidbody2D
+            rb2D = GetComponent<Rigidbody2D>();
+
+            //By storing the reciprocal of the move time we can use it by multiplying instead of dividing, this is more efficient.
+            inverseMoveTime = 1f / moveTime;
 	}
 	
 	// Update is called once per frame
@@ -27,7 +42,7 @@ public class MovingObject : MonoBehaviour {
 		Vector2 start = transform.position;
 
 		// Calculate end position based on the direction parameters passed in when calling Move.
-		Vector2 end = start + new Vector2(xDir, yDir);
+		Vector2 end = start + new Vector2(xDir *moveSpeed, yDir * moveSpeed);
 
 		//Disable the boxCollider so that linecast doesn't hit this object's own collider.
 		boxCollider.enabled = false;
@@ -41,8 +56,7 @@ public class MovingObject : MonoBehaviour {
 		//Check if anything was hit
 		if (hit.transform == null)
 		{
-			//If nothing was hit, start SmoothMovement co-routine passing in the Vector2 end as destination
-			// StartCoroutine(SmoothMovement(end));                
+			rb2D.MovePosition(end);
 
 			//Return true to say that Move was successful
 			return true;
@@ -51,6 +65,25 @@ public class MovingObject : MonoBehaviour {
 		//If something was hit, return false, Move was unsuccesful.
 		return false;
 	}
+
+	//AttemptMove takes a generic parameter T to specify the type of component we expect our unit to interact with if blocked (Player for Enemies, Wall for Player).
+	protected virtual bool AttemptMove(int xDir, int yDir, out RaycastHit2D hit)
+	{
+		//Hit will store whatever our linecast hits when Move is called.
+
+		//Set canMove to true if Move was successful, false if failed.
+		bool canMove = Move(xDir, yDir, out hit);
+
+		//Check if nothing was hit by linecast
+		if (hit.transform == null)
+			//If nothing was hit, return and don't execute further code.
+			return canMove;
+
+		return canMove;
+	}
+
+	
+    protected abstract void OnFinishedMoving();
 
 }
  
--- WonderingWorm/Assets/Scripts/WonderingWorm.cs
+++ WonderingWorm/Assets/Scripts/WonderingWorm.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WonderingWorm : MonoBehaviour {
+public class WonderingWorm : MovingObject {
 
 	// Use this for initialization
-	void Start () {
-		
+	protected override void Start () {
+    	base.Start();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		int horizontal = 0;     //Used to store the horizontal move direction.
+        int vertical = 0;       //Used to store the vertical move direction.
+			
+		//Get input from the input manager, round it to an integer and store in horizontal to set x axis move direction
+		horizontal = (int) (Input.GetAxisRaw ("Horizontal"));
 		
+		//Get input from the input manager, round it to an integer and store in vertical to set y axis move direction
+		vertical = (int) (Input.GetAxisRaw ("Vertical"));
+		
+
+		if (horizontal != 0 || vertical != 0)
+            {
+                
+
+                //Call AttemptMove passing in the generic parameter Wall, since that is what Player may interact with if they encounter one (by attacking it)
+                //Pass in horizontal and vertical as parameters to specify the direction to move Player in.
+                RaycastHit2D hit;
+                AttemptMove(horizontal, vertical, out hit);
+            }
 	}
+
+	 protected override void OnFinishedMoving()
+	{
+	}
+
 }
